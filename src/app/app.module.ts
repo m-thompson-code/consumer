5999a52b54916c3bd6a640fb353443a7ee66996b
Mark Thompson <mthompson@bitovi.com>
2023-03-28 15:47:59 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { MyEntryPointLibModule } from 'my-entry-point-lib';
+import { UiSdkModule } from 'ui-sdk-moo';
+import { ButtonModule } from 'ui-sdk-moo/button';
+import { CardComponent } from 'ui-sdk-moo/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MyEntryPointLibModule,
+    UiSdkModule,
+    ButtonModule,
+    CardComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
